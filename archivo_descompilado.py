5d4c5182becbf1bd030d664cda2b2addc493c11e
Pelayo <phuermij@myuax.com>
2024-04-19 19:48:15 +0200

    decode

---
+++ archivo_descompilado.py
@@ -0,0 +1,9 @@
+# uncompyle6 version 3.9.1
+# Python bytecode version base 3.9.0 (3425)
+# Decompiled from: Python 3.12.0 (v3.12.0:0fb18b02c8, Oct  2 2023, 09:45:56) [Clang 13.0.0 (clang-1300.0.29.30)]
+# Embedded file name: script.py
+# Compiled at: 2021-03-10 11:41:26
+# Size of source mod 2**32: 2732 bytes
+
+Unsupported Python version, 3.9.0, for decompilation
+
